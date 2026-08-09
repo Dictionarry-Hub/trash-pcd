31a3924176090febb56484f48a2936076982e8ea
Seraphys <xshatterx@protonmail.com>
2026-08-09 18:58:47 +0000

    Update SQP-4 MA Scoring

---
+++ ops/109.update-sqp-4-ma-scoring.sql
@@ -0,0 +1,223 @@
+-- @operation: export
+-- @entity: batch
+-- @name: Update SQP-4 MA Scoring
+-- @exportedAt: 2026-08-09T18:58:45.672Z
+-- @opIds: 12646, 12647, 12648, 12649, 12650, 12651, 12652, 12653, 12654, 12655, 12656, 12657, 12658, 12659, 12660, 12661, 12662, 12663, 12664, 12665, 12666, 12667, 12668, 12669
+
+-- --- BEGIN op 12646 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 310
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = '2.0 Stereo'
+  AND arr_type = 'radarr'
+  AND score = 175;
+-- --- END op 12646
+
+-- --- BEGIN op 12647 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 780
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = '5.1 Surround'
+  AND arr_type = 'radarr'
+  AND score = 365;
+-- --- END op 12647
+
+-- --- BEGIN op 12648 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 830
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = '7.1 Surround'
+  AND arr_type = 'radarr'
+  AND score = 370;
+-- --- END op 12648
+
+-- --- BEGIN op 12649 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 80
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'AAC'
+  AND arr_type = 'radarr'
+  AND score = 630;
+-- --- END op 12649
+
+-- --- BEGIN op 12650 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 480
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'ATMOS (undefined)'
+  AND arr_type = 'radarr'
+  AND score = 730;
+-- --- END op 12650
+
+-- --- BEGIN op 12651 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 3900
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'BHDStudio'
+  AND arr_type = 'radarr'
+  AND score = 3020;
+-- --- END op 12651
+
+-- --- BEGIN op 12652 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 80
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'DD'
+  AND arr_type = 'radarr'
+  AND score = 635;
+-- --- END op 12652
+
+-- --- BEGIN op 12653 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 280
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'DD+'
+  AND arr_type = 'radarr'
+  AND score = 680;
+-- --- END op 12653
+
+-- --- BEGIN op 12654 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 480
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'DD+ ATMOS'
+  AND arr_type = 'radarr'
+  AND score = 730;
+-- --- END op 12654
+
+-- --- BEGIN op 12655 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 75
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'DTS'
+  AND arr_type = 'radarr'
+  AND score = 400;
+-- --- END op 12655
+
+-- --- BEGIN op 12656 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 2510
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'DTS X'
+  AND arr_type = 'radarr'
+  AND score = 1860;
+-- --- END op 12656
+
+-- --- BEGIN op 12657 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 330
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'DTS-ES'
+  AND arr_type = 'radarr'
+  AND score = 450;
+-- --- END op 12657
+
+-- --- BEGIN op 12658 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 330
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'DTS-HD HRA'
+  AND arr_type = 'radarr'
+  AND score = 500;
+-- --- END op 12658
+
+-- --- BEGIN op 12659 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 2180
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'DTS-HD MA'
+  AND arr_type = 'radarr'
+  AND score = 1850;
+-- --- END op 12659
+
+-- --- BEGIN op 12660 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 380
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'FLAC'
+  AND arr_type = 'radarr'
+  AND score = 550;
+-- --- END op 12660
+
+-- --- BEGIN op 12661 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 3910
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'hallowed'
+  AND arr_type = 'radarr'
+  AND score = 3030;
+-- --- END op 12661
+
+-- --- BEGIN op 12662 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 510
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'HDR10'
+  AND arr_type = 'radarr'
+  AND score = 200;
+-- --- END op 12662
+
+-- --- BEGIN op 12663 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 1040
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'MA'
+  AND arr_type = 'radarr'
+  AND score = 390;
+-- --- END op 12663
+
+-- --- BEGIN op 12664 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 380
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'PCM'
+  AND arr_type = 'radarr'
+  AND score = 550;
+-- --- END op 12664
+
+-- --- BEGIN op 12665 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 1040
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'PLAY'
+  AND arr_type = 'radarr'
+  AND score = 390;
+-- --- END op 12665
+
+-- --- BEGIN op 12666 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 2180
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'TrueHD'
+  AND arr_type = 'radarr'
+  AND score = 1850;
+-- --- END op 12666
+
+-- --- BEGIN op 12667 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 2510
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'TrueHD ATMOS'
+  AND arr_type = 'radarr'
+  AND score = 1860;
+-- --- END op 12667
+
+-- --- BEGIN op 12668 ( update quality_profile "SQP-4 (MA Hybrid)" )
+UPDATE quality_profile_custom_formats
+SET score = 3970
+WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+  AND custom_format_name = 'WEBDL Boost'
+  AND arr_type = 'radarr'
+  AND score = 3040;
+-- --- END op 12668
+
+-- --- BEGIN op 12669 ( update quality_profile "SQP-4 (MA Hybrid)" )
+INSERT INTO quality_profile_custom_formats (quality_profile_name, custom_format_name, arr_type, score)
+SELECT 'SQP-4 (MA Hybrid)', '6.1 Surround', 'radarr', 780
+WHERE NOT EXISTS (
+  SELECT 1 FROM quality_profile_custom_formats
+  WHERE quality_profile_name = 'SQP-4 (MA Hybrid)'
+    AND custom_format_name = '6.1 Surround'
+    AND arr_type = 'radarr'
+);
+-- --- END op 12669
